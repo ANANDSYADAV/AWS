855fe8a62517efc40741a9dd0c21d20b20395bb0
Anand Yadav <anand_yadav@epam.com>
2025-03-11 15:54:43 +0530

    add code

---
+++ task03/app/lambdas/hello_world/index.js
@@ -0,0 +1,12 @@
+exports.handler = async (event) => {
+  // TODO implement
+  const response = {
+    statusCode: 200,
+    message: "Hello from Lambda",
+  };
+  // const response = {
+  //     "statusCode": 200,
+  //     "body": JSON.stringify('Hello from Lambda!'),
+  // };
+  return response;
+};
